fd09e2789a8615efb95532c9b2599c7f965c5c14
Didar <69292774+Didar322@users.noreply.github.com>
2024-12-18 21:42:43 +0500

    Update files.py

--- files.py
+++ files.py
@@ -6,7 +6,7 @@ import aiofiles
 
 import tensorflow as tf
 
-import random
+import hashlib
 
 # model = tf.keras.models.load_model("model.keras")
 
@@ -19,9 +19,10 @@ router = APIRouter(
 
 @router.post("/predict")
 async def create_upload_file(file: UploadFile):
+    result = int(hashlib.sha256(file.filename.encode('utf-8')).hexdigest(), 16) % 10**2
     out_file_path = '/video/'+file.filename
     async with aiofiles.open(out_file_path, 'wb') as out_file:
         while content := await file.read(1024):  # async read chunk
             await out_file.write(content)  # async write chunk
   
-    return {"Result": random.randint(55,90)/100}
+    return {"Result": result}
