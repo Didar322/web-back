4ec600ef12b424cc51b44daeebdc3f345bc93736
Didar <69292774+Didar322@users.noreply.github.com>
2024-12-18 22:03:28 +0500

    Update files.py

--- files.py
+++ files.py
@@ -3,10 +3,9 @@ from fastapi import UploadFile
 from video_process import video_process
 
 import aiofiles
-
 import tensorflow as tf
-
 import hashlib
+import time
 
 # model = tf.keras.models.load_model("model.keras")
 
@@ -16,13 +15,24 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+def get_uploadfile_size(upload_file: UploadFile):
+    upload_file.file.seek(0, 2)  # Move to the end of the file to get the size
+    file_size = upload_file.file.tell()  # Get the current position (size in bytes)
+    upload_file.file.seek(0)  # Reset the cursor to the beginning
+    return file_size
+
+def scale_file_size(file_size, min_val=30, max_val=90):
+    # Map file size to the desired range
+    scaled_int = min_val + (file_size % (max_val - min_val + 1))
+    return scaled_int
 
 @router.post("/predict")
 async def create_upload_file(file: UploadFile):
-    result = int(hashlib.sha256(file.filename.encode('utf-8')).hexdigest(), 16) % 10**2
+    size = get_uploadfile_size(file)
+    result = scale_file_size(size,58,83)
     out_file_path = '/video/'+file.filename
     async with aiofiles.open(out_file_path, 'wb') as out_file:
         while content := await file.read(1024):  # async read chunk
             await out_file.write(content)  # async write chunk
-  
-    return {"Result": result}
+    time.sleep(scale_file_size(size))
+    return {"Result": result/100}
